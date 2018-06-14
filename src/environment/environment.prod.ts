065b8e178aa2a966d8e2d9e183f3abf16294c41e
Sauli Rodriguez <sauli@e-libro.com>
2018-06-13 22:46:53 -0600

    Added ng cli project

--- src/environment/environment.prod.ts
+++
@@ -1,9 +0,0 @@
-import { Environment } from './environment.interface';
-
-export const environment: Environment = {
-    target: 'prod',
-    server: {
-        host: 'localhost',
-        port: 9000
-    }
-};
