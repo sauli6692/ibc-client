7be124666d918bec7ee93d9a11ef20690f3a2e2e
Sauli Rodriguez <sauli@e-libro.com>
2018-07-12 17:07:53 -0600

    Added sort, select and expandable row. Style fixes

--- src/app/main/components/home/home.component.ts
+++ src/app/main/components/home/home.component.ts
@@ -14,56 +14,56 @@ export class HomeComponent implements OnInit {
     tiles: CoreTile[];
     items = [];
     _items = [{
-        'name': '1 Aaron 2Moore',
+        'name': 'Aaron 2Moore',
         'email': 'Heath44@hotmail.com',
         'jobTitle': 'Regional Configuration Producer',
         'active': true,
         'phoneNumber': '611-898-6201',
         'date': '2015-11-06T07:21:25.510Z'
     }, {
-        'name': '2 Yvonne Conroy Mrs.',
+        'name': 'Yvonne Conroy Mrs.',
         'email': 'Gideon9@yahoo.com',
         'jobTitle': 'Global Mobility Orchestrator',
         'active': false,
         'phoneNumber': '115-850-0969',
         'date': '2014-12-20T00:48:40.276Z'
     }, {
-        'name': '3 Laron Padberg',
+        'name': 'Laron Padberg',
         'email': 'Laney_Huels@hotmail.com',
         'jobTitle': 'Senior Directives Supervisor',
         'active': false,
         'phoneNumber': '632-654-3034',
         'date': '2015-09-29T04:33:38.544Z'
     }, {
-        'name': '4 Aaron 2Moore',
+        'name': 'Aaron 2Moore 2',
         'email': 'Heath44@hotmail.com',
         'jobTitle': 'Regional Configuration Producer',
         'active': true,
         'phoneNumber': '611-898-6201',
         'date': '2015-11-06T07:21:25.510Z'
     }, {
-        'name': '5 Yvonne Conroy Mrs.',
+        'name': 'Yvonne Conroy Mrs. 2',
         'email': 'Gideon9@yahoo.com',
         'jobTitle': 'Global Mobility Orchestrator',
         'active': false,
         'phoneNumber': '115-850-0969',
         'date': '2014-12-20T00:48:40.276Z'
     }, {
-        'name': '6 Laron Padberg',
+        'name': 'Laron Padberg 2',
         'email': 'Laney_Huels@hotmail.com',
         'jobTitle': 'Senior Directives Supervisor',
         'active': false,
         'phoneNumber': '632-654-3034',
         'date': '2015-09-29T04:33:38.544Z'
     }, {
-        'name': '7 Laron Padberg',
+        'name': 'Laron Padberg 3',
         'email': 'Laney_Huels@hotmail.com',
         'jobTitle': 'Senior Directives Supervisor',
         'active': false,
         'phoneNumber': '632-654-3034',
         'date': '2015-09-29T04:33:38.544Z'
     }, {
-        'name': '8 Laron Padberg',
+        'name': 'Laron Padberg 4',
         'email': 'Laney_Huels@hotmail.com',
         'jobTitle': 'Senior Directives Supervisor',
         'active': false,
@@ -95,6 +95,9 @@ export class HomeComponent implements OnInit {
             );
         } else {
             items = this._items;
+        }
+        if (params.sortBy) {
+            items = _.orderBy(items, [params.sortBy], [params.sortAsc ? 'asc' : 'desc']);
         }
         this.items = items.slice(params.offset, params.offset + params.limit);
         this.itemCount = items.length;
